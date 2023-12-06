ae6b0e5ff7c0dd70801e8061dbf98d996ea98155
auroraphtgrp01 <minhtuanledng@gmail.com>
2023-12-06 16:02:03 +0700

    C19.201 -- Socket.io & Private Message

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,14 @@ import { initFolder } from './utils/file'
 import { config } from 'dotenv'
 import cors from 'cors'
 import { RouterApp } from './routes/index.routes'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 // import 'src/utils/faker'
 import './utils/s3'
 config()
 const app = express()
 const port = process.env.PORT || 4000
+const httpServer = createServer(app)
 
 initFolder()
 app.use(cors())
@@ -24,6 +27,40 @@ app.use(express.json())
 RouterApp(app)
 app.use(defaultErrorHandler)
 
-app.listen(port, () => {
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`)
+// })
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000'
+  }
+})
+const users: {
+  [key: string]: {
+    socket_id: string
+  }
+} = {}
+io.on('connection', (socket) => {
+  console.log(`${socket.id} connected`)
+  const user_id = socket.handshake.auth._id
+  users[user_id] = {
+    socket_id: socket.id
+  }
+  console.log('users', users)
+
+  socket.on('disconnect', () => {
+    delete users[user_id]
+    console.log(`${socket.id} disconnected`)
+  })
+  socket.on('private_message', (data) => {
+    const receive_socket_id = users[data.to].socket_id
+    socket.to(receive_socket_id).emit('private_message_rec', {
+      content: data.content,
+      from: user_id 
+    })
+  })
+})
+
+httpServer.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
